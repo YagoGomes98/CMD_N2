5b1078103694de8cd7a57077cdc96cd244fe07cd
Yago <yagobbgomes@gmail.com>
2021-12-02 23:03:30 -0300

    commit trabalho N2

---
+++ app/src/main/java/com/example/apptrabalhon1/Filme.java
@@ -0,0 +1,40 @@
+package com.example.apptrabalhon1;
+
+public class Filme {
+
+    public static final int ANO_MINIMO = 1950;
+
+    public int id;
+    public String nome;
+    private int ano;
+
+    public Filme() {
+
+    }
+
+    public Filme(String nome, int ano) {
+        this.nome = nome;
+        this.setAno( ano );
+    }
+
+    public Filme(int id, String nome, int ano) {
+        this.id = id;
+        this.nome = nome;
+        this.setAno(ano);
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        if( ano >= ANO_MINIMO )
+            this.ano = ano;
+
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + nome + " | " + ano;
+    }
+}
